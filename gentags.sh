f2444a6eb21029dfd6eec6fd4dcdc52cbdc20292
Radu Berinde <radu.berinde@gmail.com>
2016-01-26 12:41:09 -0500

    Updates and notion

--- gentags.sh
+++ gentags.sh
@@ -4,7 +4,7 @@ set -e
 cd /go/src/github.com/cockroachdb
 /go/bin/gotags -R . > tags 2>/root/gentags.log
 
-if [ -d /go2]; then
+if [ -d /go2 ]; then
     cd /go2/src/github.com/cockroachdb
     /go/bin/gotags -R . > tags 2>>/root/gentags.log
 fi
