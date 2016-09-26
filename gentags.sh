87ac6f60aa4b2c47bc2bbc43676fdbf9df709032
Radu Berinde <radu.berinde@gmail.com>
2016-09-26 14:35:10 -0400

    better gentags

--- gentags.sh
+++ gentags.sh
@@ -1,10 +1,23 @@
 #!/bin/sh
 
-set -e
-cd /go/src/github.com/cockroachdb
-/go/bin/gotags -R . > tags 2>~/gentags.log
+GOSRC_DIRS=/go*/src
+REL_PATHS="github.com/cockroachdb golang.org/x github.com/opentracing github.com/lightstep"
+LOGFILE=~/gentags.log
 
-if [ -d /go2 ]; then
-    cd /go2/src/github.com/cockroachdb
-    /go/bin/gotags -R . > tags 2>>~/gentags.log
-fi
+set -e
+
+echo "REL_PATHS: $REL_PATHS" > $LOGFILE
+for gosrc in $GOSRC_DIRS; do
+  # Leave a tags file in each directory.
+  for path in $REL_PATHS; do
+    cd $gosrc/$path
+    echo "Running in `pwd -P`" >> $LOGFILE
+    /go/bin/gotags -R . > tags 2>>$LOGFILE
+  done
+
+  # Also leave a "root" tags file. This file is consulted if an identifier is
+  # not found in a tags file inside a more specific subdir.
+  cd $gosrc
+  echo "Running in `pwd -P`" >> $LOGFILE
+  /go/bin/gotags -R $REL_PATHS > tags 2>>$LOGFILE
+done
