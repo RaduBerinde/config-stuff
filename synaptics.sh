2c2ff0341f648b023c9ddb59630bc55161c1e305
Radu Berinde <radu@cockroachlabs.com>
2017-12-12 06:45:32 -0500

    mint mouse stuff

---
+++ synaptics.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# To get this to work:
+#  - in /etc/modprobe.d/blacklist.conf add:
+#      # Remove SynPS/2 Synaptics Touchpad driver.
+#      blacklist psmouse
+#
+#  - in /usr/share/X11/xorg.conf.d, move 50-synaptics to 95-synaptics
+#    (so it's after libinput and is preferred).
+
+
+synclient TapButton2=2
+synclient TapButton3=3
+synclient ClickFinger2=2
+synclient ClickFinger3=3
+synclient PalmDetect=1
+synclient PalmMinWidth=4
+synclient PalmMinZ=50
